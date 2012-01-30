6d14ab0bc9e8acbe59c9fd33d98d38cf28dc2ace
Kristoffer Andersen <kja@cs.au.dk>
2012-01-30 18:49:10 +0100

    Added variable number of iterations

---
+++ artemis-code/src/cookies/immutablecookiejar.cpp
@@ -0,0 +1,5 @@
+#include "immutablecookiejar.h"
+
+immutablecookiejar::immutablecookiejar()
+{
+}
---
+++ artemis-code/src/cookies/immutablecookiejar.h
@@ -0,0 +1,10 @@
+#ifndef IMMUTABLECOOKIEJAR_H
+#define IMMUTABLECOOKIEJAR_H
+
+class immutablecookiejar
+{
+public:
+    immutablecookiejar();
+};
+
+#endif // IMMUTABLECOOKIEJAR_H
