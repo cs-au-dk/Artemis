34e3da9a46ac76e69628800624c4e076feecfdc7
Casper S. Jensen <casper@svenningjensen.dk>
2012-02-20 18:19:49 +0100

    snaptshot

--- ailproxy/ail-module/run.js
+++ ailproxy/ail-module/run.js
@@ -1,3 +1,8 @@
 var ailreader = require('./build/Release/ailreader');
 
-console.warn(ailreader.generate_response_permutation('foo','bar'));
+url = ['ajax', 'search'];
+kwargs = ['query'];
+vwargs = ['something'];
+ail_file = '/home/semadk/src/svn/ail/benchmarks/experiments/learning/example-ail-specs/simpleajax.ail';
+
+console.warn(ailreader.generate_response_permutation(url, kwargs, vwargs, ail_file));
