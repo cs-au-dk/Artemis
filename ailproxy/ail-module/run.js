24467472b3c3903075d2e6665a40496a3ccbcca7
Kristoffer Andersen <kja@cs.au.dk>
2012-03-01 12:40:01 +0100

    Added persistence to the AIL nodeJS module

--- ailproxy/ail-module/run.js
+++ ailproxy/ail-module/run.js
@@ -1,8 +1,10 @@
-var ailreader = require('./build/Release/ailreader');
+var ail = require('./build/Release/AIL');
 
-url = ['ajax', 'search'];
-kwargs = ['query'];
-vwargs = ['something'];
-ail_file = '/home/semadk/src/svn/ail/benchmarks/experiments/learning/example-ail-specs/simpleajax.ail';
+var url = ['ajax', 'search'];
+var kwargs = ['query'];
+var vwargs = ['something'];
+var schema_path = './tests/schema.ail';
 
-console.warn(ailreader.generate_response_permutation(url, kwargs, vwargs, ail_file));
+var reader = new ail.Reader(schema_path);
+console.log(reader.generateResponse(url, kwargs, vwargs));
+console.log(reader.generateResponse(url, kwargs, vwargs));
