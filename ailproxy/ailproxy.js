307277c741ccaa17be477377edb91f57805a3d86
Casper S. Jensen <casper@svenningjensen.dk>
2012-04-03 21:04:01 +0200

    Added new random mode

--- ailproxy/ailproxy.js
+++ ailproxy/ailproxy.js
@@ -6,10 +6,124 @@ var fs = require('fs');
 var path = require('path');
 
 var server_only_mode = false;
+var random_mode = false;
 var server_base_dir = null;
 var server_cache = {};
 
 var AILReader;
+
+var MAX_RANDOM_INT = 200;
+var MAX_RANDOM_ARRAY = 20;
+var MAX_RANDOM_STRING = 8;
+var MAX_RANDOM_PROPERTIES = 8;
+
+function randInt(max_value) {
+	value = Math.floor(Math.random() * (max_value+1));
+
+	if (value > max_value) {
+		// Math.random can return 1
+		// avoid returning max_value+1 in that case
+		return max_value; 
+	}
+
+	return value;
+}
+
+function randomJson() {
+
+	var generators;
+
+	function getGenerator(max) {
+		range = generators.length - 1;
+
+		if (max != undefined) {
+			range = max;
+		}
+
+		return generators[randInt(range)];
+	}
+
+	var rand_chars = "abcdefghijklmnopqrstuvwxyz";
+
+	function _randomString() {
+	    var text = new Array();
+	    
+	    for (i = 0; i < MAX_RANDOM_STRING; i++) {
+	        text.push(rand_chars.charAt(randInt(rand_chars.length - 1)));
+	    }
+
+	    return text.join('');
+	}
+
+	generators = [
+		function() {
+			// array
+			array = new Array();
+
+			num_elements = randInt(MAX_RANDOM_ARRAY);
+
+			if (num_elements == 0) {
+				return '[]';
+			}
+
+			generator = getGenerator();
+
+			for (i = 0; i < num_elements; i++) {
+				array.push(',')
+				array.push(generator());
+			}
+
+			array[0] = "["; // replace first "," with a [
+			array.push("]");
+
+			return array.join('');
+		},
+		function() {
+			// object
+			obj = new Array();
+
+			num_properties = randInt(MAX_RANDOM_PROPERTIES);
+
+			if (num_properties == 0) {
+				return '{}';
+			}
+
+			for (i = 0; i < num_properties; i++) {
+				obj.push(',');
+				obj.push('"' + _randomString() + '":');
+				obj.push(getGenerator()());
+			}
+
+			obj[0] = '{'; // replace first , with a {
+			obj.push('}');
+
+			return obj.join('');
+		},
+		function() {
+			// boolean
+			values = ["True", "False"];
+			return values[randInt(1)];
+		},
+		function() {
+			// integer
+			return '' + Math.floor(Math.random() * MAX_RANDOM_INT);
+		},
+		function() {
+			// string
+			return '<string>';
+		},
+		function() {
+			// null
+			return 'null';
+		},
+		function() {
+			// float
+			return '' + Math.random();
+		}
+	];
+
+	return getGenerator(1)();
+}
 
 function extractKeyset(assoArray) {
     if (assoArray.length == 0) {
@@ -90,14 +204,29 @@ function requestHandler(request, response) {
 
 		if (ailResponse != undefined) {
 			
-			console.log('AIL ', request.url);
+			if (random_mode == false) {
+				
+				console.log('AIL ', request.url);
 
-			response.writeHead(200, {
-		    'Content-Length' : ailResponse.length,
-		    'Content-Type'   : 'application/json'});
-		
-			response.write(ailResponse);
-			response.end();
+				response.writeHead(200, {
+			    'Content-Length' : ailResponse.length,
+			    'Content-Type'   : 'application/json'});
+			
+				response.write(ailResponse);
+				response.end();
+
+			} else {
+
+				console.log('AIL-RANDOM ', request.url);
+
+				response.writeHead(200, {
+			    'Content-Type'   : 'application/json'});
+				
+				random_json = randomJson();
+
+				response.write(random_json);
+				response.end();				
+			}
 		
 		} else if (server_only_mode) {
 
@@ -169,9 +298,10 @@ function requestHandler(request, response) {
 }
 
 if (process.argv.length < 3) {
-    console.log('Error, proper usage: node ailproxy.js /path/to/schema [--server-only-mode /path/to/files]');
+    console.log('Error, proper usage: node ailproxy.js /path/to/schema [--server-only-mode /path/to/files | --random-mode]');
 } else {
 	server_only_mode = (process.argv.length > 3 && process.argv[3] == '--server-only-mode');
+	random_mode = (process.argv.length > 3 && process.argv[3] == '--random-mode');
 
 	if (process.argv.length > 4) {
 		server_base_dir = process.argv[4];
