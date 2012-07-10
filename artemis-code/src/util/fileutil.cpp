cbff0f32cb7132b915e800a5ef7e11f11b00c75f
kja <kja@kja-VirtualBox.(none)>
2012-07-10 12:55:31 +0200

    adding lost files from artemis-code

---
+++ artemis-code/src/util/fileutil.cpp
@@ -0,0 +1,30 @@
+#include "fileutil.h"
+#include <QFile>
+#include <QDir>
+#include <QTextStream>
+
+namespace artemis {
+
+void write_string_to_file(QString filename, QString data) {
+    QFile file(filename);
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&file);
+    out << (data.isEmpty() ? " " : data);
+    file.close();
+    out.flush();
+}
+
+void create_dir(QString path, QString folder_name){
+    if (QDir().exists(path + "/" + folder_name))
+        return;
+    QDir().mkdir(path + "/" + folder_name);
+}
+
+QString read_file(QString file_s) {
+    QFile f(file_s);
+    f.open(QFile::ReadOnly | QFile::Text);
+    QTextStream in(&f);
+    return in.readAll();
+}
+
+}
