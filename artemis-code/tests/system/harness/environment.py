d0f328985859f78726a7155ca04591878ee75408
Christian Budde Christensen <christi@n-bud.de>
2013-03-04 11:45:06 +0100

    Changed server from python SimpleHTTPServer to PHP server. This enables php in tests and it seems to hang less than the python server.

--- artemis-code/tests/system/harness/environment.py
+++ artemis-code/tests/system/harness/environment.py
@@ -2,12 +2,13 @@ import subprocess
 import os
 import signal
 import time
+import re
 
 class WebServer(object):
 
-    def __init__(self, web_root, port):
-        cmd = 'python -m SimpleHTTPServer %s' % port
-
+    def __init__(self, web_root,port): 
+        #cmd = 'python -m SimpleHTTPServer %s ' % port
+        cmd = 'php -S localhost:%s >/dev/null 2>/dev/null' % port
         self._webserver = subprocess.Popen(cmd, 
             cwd=web_root, 
             shell=True, 
