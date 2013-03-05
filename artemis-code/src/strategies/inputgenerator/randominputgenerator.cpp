06d586f54fbc2a0c6e2aa074d35d7e4a616447e2
Casper S. Jensen <casper@svenningjensen.dk>
2013-03-05 09:45:24 +0100

    Re-enable same length configuration generation

--- artemis-code/src/strategies/inputgenerator/randominputgenerator.cpp
+++ artemis-code/src/strategies/inputgenerator/randominputgenerator.cpp
@@ -54,7 +54,7 @@ QList<QSharedPointer<ExecutableConfiguration> > RandomInputGenerator::addNewConf
 
     QList<QSharedPointer<ExecutableConfiguration> > newConfigurations;
 
-    //newConfigurations.append(insertSameLength(configuration, result));
+    newConfigurations.append(insertSameLength(configuration, result));
     newConfigurations.append(insertExtended(configuration, result));
 
     return newConfigurations;
