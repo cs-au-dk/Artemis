b5dbef8fe803f030f50890e6db8d2c8a932f5904
Christian Budde Christensen <budde377@cs.au.dk>
2013-03-22 15:14:16 +0100

    started devl. of better html reports

--- artemis-code/src/model/coverage/coveragetooutputstream.cpp
+++ artemis-code/src/model/coverage/coveragetooutputstream.cpp
@@ -62,9 +62,21 @@ void writeCoverageHtml(CoverageListenerPtr cov)
 
     QDir appdir("", "*.html", QDir::Time);
     QStringList existingFiles = appdir.entryList();
-
-    QString res = "<html><head><meta charset=\"utf-8\"/><title>Test</title><style>";
-    res += "table { border-collapse: collapse; } td.covered { background-color: #00FF00; } td.uncovered { background-color: #FF0000; }</style></head><body>";
+    QString timeString = QDateTime::currentDateTime().toString("dd-MM-yy-hh-mm-ss") ;
+    QString res = "<html><head><meta charset=\"utf-8\"/><title>Coverage Report ("+timeString+")</title>";
+    res += "\n";
+    res += "<script type='text/javascript' src='https://google-code-prettify.googlecode.com/svn/loader/prettify.js'></script>";
+    res += "\n";
+    res += "<script src='http://code.jquery.com/jquery-latest.min.js'></script>";
+    res += "\n";
+    res += "<script type='text/javascript'> $.fn.updateOLOffset = function(){ if($(this).hasClass('startline')){var nr = $(this).attr('class').replace(/.*startlinenr\\[([0-9]+)\\].*/, '$1'); $(this).find('ol').attr('start',nr); $(this).removeClass('startline');}}; ";
+    res += "$(document).ready(function(){  $('.expandLink').click(function(){  var pre = $(this).parent().find('pre');  var self = $(this); if(self.hasClass('expanded')){  pre.hide();  self.removeClass('expanded');  self.text('show code');} else {  pre.show();  if(!pre.hasClass('prettyprinted')){  pre.addClass('prettyprint');  prettyPrint(function(){  pre.removeClass('prettyprint');  pre.updateOLOffset();  });  }  self.addClass('expanded');  self.text('hide');  }  });});</script>";
+    res += "\n";
+    res += "<link rel='stylesheet' type='text/css' href='https://google-code-prettify.googlecode.com/svn/loader/prettify.css'>";
+    res += "\n";
+    res += "<style> .linenums > ol > li:nth-of-type(5n), .linenums > ol > li:nth-of-type(1) {list-style-type: decimal !important;} .linenums > ol > li {list-style-type: none;} pre {padding: 2px; border: 1px solid #888; display:none;} .expandLink{float:right;} pre > ol {overflow-x:auto;}</style>";
+    res += "\n";
+    res += "</head><body>";
 
     if (!existingFiles.isEmpty()) {
         res += "<a href=\"" + existingFiles.at(0) + "\">Previous run</a>";
@@ -73,26 +85,22 @@ void writeCoverageHtml(CoverageListenerPtr cov)
     foreach(int sourceID, cov->getSourceIDs()) {
 
         QSet<uint> lineCoverage = cov->getSourceInfo(sourceID)->getLineCoverage();
-
+        res += "<div>";
+        res += "<a href='#?' class='expandLink'>show</a>";
         res += "<h2>" + Qt::escape(cov->getSourceInfo(sourceID)->getURL()) + "</h2>";
-        res += "<pre><table>";
-
         int startline = cov->getSourceInfo(sourceID)->getStartLine();
+        res += "<pre class='linenums "+(startline >1 ? "startline startlinenr["+QString::number(startline)+"]":"")+"'>";
+
         foreach(QString line, cov->getSourceInfo(sourceID)->getSource().split("\n", QString::KeepEmptyParts)) {
-            QString s = QTextDocument(line).toHtml();
-            int p = s.indexOf("<p");
-            QString htmlString = s.mid(p,s.lastIndexOf("</p>")-p+4);
-            res += "<tr><td>" + QString::number(startline) + "</td><td class=\""
-                   + QString(lineCoverage.contains(startline) ? "covered" : "uncovered")
-                   + "\">" + htmlString + "</td></tr>";
-            startline += 1;
+            res += QTextDocument(line).toPlainText().replace("<","&lt;").replace(">","&gt;") + "&nbsp;\n";
+
         }
-        res += "</table></pre>";
+        res += "</pre></div>";
 
     }
     res += ("</body></html>");
 
-    QString pathToFile = QString("coverage-") + QDateTime::currentDateTime().toString("dd-MM-yy-hh-mm-ss") + ".html";
+    QString pathToFile = QString("coverage-") + timeString + ".html";
 
     writeStringToFile(pathToFile, res);
 }
