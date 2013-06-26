cs-au-dk/Artemis

commit 6a99f6d0591fa2dfc9d5e1df6ab77680c3dd9b33

Added more liberal handling of formfields

artemis-code/src/runtime/input/forms/formfield.cpp
@@ -88,8 +88,6 @@ FormFieldTypes getTypeFromAttr(QString typeAttr)
 
     if (typeAttr == "password"
         || typeAttr == "text"
-        || typeAttr == "textbox" //YAHOO FIX
-        || typeAttr == "input" //CHINAZ.COM fix
         || typeAttr == "email" //HTML5
         || typeAttr == "file"
         || typeAttr == "search" //HTML5
@@ -107,7 +105,7 @@ FormFieldTypes getTypeFromAttr(QString typeAttr)
             )
         { return TEXT; }
 
-    qDebug() << "Unknown type attribute on form element: "<< typeAttr;
-    assert(false);
+    qWarning(); << "WARN: Unknown type attribute on form element: "<< typeAttr;
+    return TEXT;
 }
 }

commit 703b905f552956de8083cb0732bf0d90c0b45369

Fix bug introduced by 6a99f6d059

artemis-code/src/runtime/input/forms/formfield.cpp
@@ -105,7 +105,7 @@ FormFieldTypes getTypeFromAttr(QString typeAttr)
             )
         { return TEXT; }
 
-    qWarning(); << "WARN: Unknown type attribute on form element: "<< typeAttr;
+    qWarning() << "WARN: Unknown type attribute on form element: " << typeAttr;
     return TEXT;
 }
 }