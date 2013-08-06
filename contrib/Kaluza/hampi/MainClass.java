b9a3027f540aec9d085f538f6b756f132e1cf713
Casper Svenning Jensen <casper@svenningjensen.dk>
2013-08-06 11:26:45 +0200

    Remove Kaluza solver

--- contrib/Kaluza/hampi/MainClass.java
+++
@@ -1,7 +0,0 @@
-public class MainClass {
-  public static void main(String args[]) {
-    int i = 11;
-    System.out.println("Hex is " + Integer.toHexString(i));
-  }
-}
-
--- contrib/Kaluza/hampi/lib/foo/src/lexer.mli
+++
@@ -1,3 +0,0 @@
-val __ocaml_lex_tables : Lexing.lex_tables
-val token : Lexing.lexbuf -> Parser.token
-val __ocaml_lex_token_rec : Lexing.lexbuf -> int -> Parser.token
