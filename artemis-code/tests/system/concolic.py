d7400f43854b9e5409f3313fe969e756b9fae2da
Christian Budde Christensen <budde377@cs.au.dk>
2013-10-09 10:10:27 +0200

    Started development of better assertions for concolic tests

--- artemis-code/tests/system/concolic.py
+++ artemis-code/tests/system/concolic.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 
-WEBSERVER_PORT = 8001
+WEBSERVER_PORT = 8002
 WEBSERVER_ROOT = './fixtures/concolic/'
 WEBSERVER_URL = 'http://localhost:%s' % WEBSERVER_PORT
 
 import sys
-import nose
 import re
-import subprocess
+import unittest
 
 from harness.environment import WebServer
 from harness.artemis import execute_artemis
 from os import listdir
 from os.path import isfile, join
 
-def _run_test(test_filename, dryrun=False):
-    
+
+class TestSequence(unittest.TestCase):
+    pass
+
+
+"""
+def _run_test(test_filename, pc=None, dryrun=False):
     name = test_filename.replace('.', '_')
-    
-    report = execute_artemis(name, "%s/%s" % (WEBSERVER_URL, test_filename), 
+
+    report = execute_artemis(name, "%s/%s" % (WEBSERVER_URL, test_filename),
                              iterations=2,
                              major_mode='concolic',
                              dryrun=dryrun)
@@ -26,13 +30,19 @@ def _run_test(test_filename, dryrun=False):
     if dryrun:
         # only print the command, exit
         return
-        
+    assert pc is None
+    if pc is not None:
+        assert len(report['pathCondition']) > 0, "No path condition to compare with"
+        assert report['pathCondition'][0].replace(" ", "") == pc.replace(" ", "")
+
     # insert global assertions here
 
-    assert report.get('FormCrawl::Entrypoints', 0) > 0, "No entrypoints found, check if the HTTP server is serving the right folder"
+    assert report.get('FormCrawl::Entrypoints',
+                      0) > 0, "No entrypoints found, check if the HTTP server is serving the right folder"
     assert report.get('Concolic::Solver::ConstraintsSolvedAsUNSAT', 0) == 0, "An UNSAT constraint were encountered"
     assert report.get('Concolic::Solver::ConstraintsSolved', 0) != 0, "No constraints were solved"
-    assert report.get('Concolic::Solver::ConstraintsNotWritten', 0) == 0, "Unwritten constraints were encountered"            
+    assert report.get('Concolic::Solver::ConstraintsNotWritten', 0) == 0, "Unwritten constraints were encountered"
+"""
 
 
 def _list_tests_in_folder(folder):
@@ -40,28 +50,93 @@ def _list_tests_in_folder(folder):
 
     for f in listdir(folder):
         p = join(folder, f)
-
-        if not isfile(p) or f[0:1] == "_" or f[0:1] == "%" or '~' in f or '#' in f:
+        if not isfile(p) or f[0:1] == "_" or f[0:1] == "%" or not f[-5:] == '.html':
             continue
-        
-        out.append(f)
+        result = {"test": {}, "i_test": {}, "fn": f}
+        with open(p, 'r') as fl:
+            if re.match("^\s*<!--\s*$", fl.readline()):
+                for line in fl:
+                    m = re.match("\s*TEST(_INTERN)? ([^<>!=\s]+) ((<|>|=|!)=?) (.+)$", line)
+                    if not m:
+                        continue
+                    test_mode = "i_test" if m.group(1) else "test"
+                    op = {"<": "lt", "<=": "leq", ">": "gt", ">=": "geq", "==": "eq", "=": "eq", "!": "neq",
+                          "!=": "neq"}[m.group(3)]
+                    if op not in result[test_mode]:
+                        result[test_mode][op] = {}
+                    result[test_mode][op][m.group(2).strip()] = m.group(5).strip()
+                    if re.match("-->", line):
+                        break
 
+        if len(result['test']) == 0:
+            result['test'] = None
+        if len(result['i_test']) == 0:
+            result['i_test'] = None
+        out.append(result)
     return out
 
 
-def test_generator():
-    server = WebServer(WEBSERVER_ROOT, WEBSERVER_PORT)
-    for t in _list_tests_in_folder(WEBSERVER_ROOT):
-	yield _run_test, t
-    del server
+def test_generator(filename, name, test_dict=None, internal_test=None, dry_run=False):
+    def test(self):
+        report = execute_artemis(name, "%s/%s" % (WEBSERVER_URL, filename),
+                                 iterations=2,
+                                 major_mode='concolic',
+                                 dryrun=dry_run)
+
+        if dry_run:
+            # only print the command, exit
+            return
+
+        assert test_dict or internal_test, "No tests to execute"
+        if internal_test:
+            for op, tMap in internal_test.iteritems():
+                for s, v in tMap.iteritems():
+                    _assert_test_case(self, op, _get_from_report(report, s)['val'], _get_from_report(report, v)['val'])
+        if test_dict:
+            for op, tMap in test_dict.iteritems():
+                for s, v in tMap.iteritems():
+                    v = int(v) if v.isdigit() else (True if v == "true" else (False if v == "false" else v))
+                    r_val = _get_from_report(report, s)
+                    _assert_test_case(self, op, r_val['val'], v.replace(" ", "") if r_val['pc'] else v)
+
+    return test
+
+
+def _get_from_report(report, key):
+    m = re.match('PC(\[([0-9]+)\])?', key)
+    if m:
+        index = int(m.group(2)) if m.group(2) else 0
+        pc = report['pathCondition']
+        assert len(pc) >= index + 1, "Not enough path conditions generated by artemis"
+        return {"val": pc[index].replace(" ", ""), "pc": True}
+    else:
+        assert key in report, "Could not test for %s. Index not found in report" % key
+        return {"val": report[key], "pc": False}
+
+
+def _assert_test_case(test_case, op, v1, v2):
+    if op == "eq":
+        test_case.assertEqual(v1, v2)
+    elif op == "neq":
+        test_case.assertNotEqual(v1, v2)
+    elif op == "geq":
+        test_case.assertGreaterEqual(v1, v2)
+    elif op == "gt":
+        test_case.assertGreater(v1, v2)
+    elif op == "leq":
+        test_case.assertLessEqual(v1, v2)
+    elif op == "lt":
+        test_case.assertLess(v1, v2)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        subprocess.call(['nosetests', 'concolic.py'])
-    else:
-        server = WebServer(WEBSERVER_ROOT, WEBSERVER_PORT)
-        dryrun = len(sys.argv) == 3 and sys.argv[2] == "dryrun"
-        _run_test(sys.argv[1], dryrun=dryrun)
-        del server
-
+    server = WebServer(WEBSERVER_ROOT, WEBSERVER_PORT)
+    dryrun = len(sys.argv) == 2 and sys.argv[1] == "dryrun"
+    for t in _list_tests_in_folder(WEBSERVER_ROOT):
+        test_name = 'test_%s' % t['fn'].replace(".", "_")
+        test = test_generator(t['fn'], test_name, test_dict=t['test'], internal_test=t['i_test'],
+                              dry_run=dryrun)
+        setattr(TestSequence, test_name, test)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSequence)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    del server
