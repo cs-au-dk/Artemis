848642905304bb455b672c334674f653fde0ebb6
Ben Spencer <benjamin.spencer@balliol.ox.ac.uk>
2014-04-26 18:14:27 +0100

    Updated CVC4 version.

--- contrib/CVC4/timed-cvc4.sh
+++ contrib/CVC4/timed-cvc4.sh
@@ -2,5 +2,5 @@
 
 # Run CVC4 with a 10 minute timeout.
 
-timeout 600 ${ARTEMISDIR}/contrib/CVC4/cvc4-2014-03-01-x86_64-linux-opt "$@"
+timeout 600 ${ARTEMISDIR}/contrib/CVC4/cvc4-2014-04-26-x86_64-linux-opt "$@"
 
