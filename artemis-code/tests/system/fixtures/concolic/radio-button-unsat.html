cs-au-dk/Artemis

commit e52d53b97651dc648e886d4450e6bb23fd9d56fb

Concolic mode benchmarking: Allow DOM constraints features to be disabled.

artemis-code/src/concolic/solver/constraintwriter/cvc4.cpp
@@ -70,7 +70,7 @@ void CVC4ConstraintWriter::preVisitPathConditionsHook(QSet<QString> varsUsed)
     //mOutput << "(set-option :finite-model-find true)" << std::endl;
     mOutput << std::endl;
 
-    // Only write the form frestrictions which relate to variables which are actually used in the PC.
+    // Only write the form restrictions which relate to variables which are actually used in the PC.
     foreach(SelectRestriction sr, mFormRestrictions.first) {
         // TODO: Hack to guess the variable names, as in helperSelectRestriction().
         QString name = QString("SYM_IN_%1").arg(sr.variable);

artemis-code/src/runtime/options.h
@@ -21,6 +21,7 @@
 #include <QSet>
 #include <QMap>
 #include <QString>
+#include <QFlags>
 #include "input/forms/injectionvalue.h"
 
 namespace artemis
@@ -70,6 +71,10 @@ struct ConcolicSearchSelector
 {
      enum { SELECTOR_DFS, SELECTOR_RANDOM, SELECTOR_AVOID_UNSAT, SELECTOR_ROUND_ROBIN } type;
      QList<ConcolicSearchSelector> components;
+};
+
+enum ConcolicBenchmarkFeatures {
+    RADIO_RESTRICTION, SELECT_RESTRICTION, SELECT_RESTRICTION_DYNAMIC
 };
 
 
@@ -147,6 +152,8 @@ typedef struct OptionsType {
     int heapReportFactor;
 
     QString customUserAgent;
+
+    QFlags<ConcolicBenchmarkFeatures> concolicDisabledFeatures;
 
 } Options;
 

artemis-code/src/artemis.cpp
@@ -147,9 +147,14 @@ QUrl parseCmd(int argc, char* argv[], artemis::Options& options)
             "           Outputs a graph of the symbolic variables which are read from each event handler.\n"
             "           (Requires major-mode concolic and concolic-event-sequences)\n"
             "\n"
+            "--concolic-disable-features <features-list>\n"
+            "           Used for benchmarking only. Disables the listed features (comma separated list).\n"
+            "           The features which can be disabled with this option are:\n"
+            "           radio-restriction, select-restriction, select-restriction-dynamic\n"
+            "\n"
             "--smt-solver <solver>:\n"
             "           z3str - Use the Z3-str SMT solver as backend.\n"
-            "           cvc4 (default) - Use the CVC4 SMT solver as backend. CVC4 is required to be on your path.\n"
+            "           cvc4 (default) - Use the CVC4 SMT solver as backend.\n"
             "           kaluza - Use the Kaluza solver as backend.\n"
             "\n"
             "--strategy-priority <strategy>:\n"
@@ -204,6 +209,7 @@ QUrl parseCmd(int argc, char* argv[], artemis::Options& options)
     {"concolic-event-sequences", required_argument, NULL, 'w'},
     {"concolic-event-sequence-permutation", required_argument, NULL, 'W'},
     {"concolic-event-handler-report", no_argument, NULL, 'H'},
+    {"concolic-disable-features", required_argument, NULL, 'B'},
     {"smt-solver", required_argument, NULL, 'n'},
     {"export-event-sequence", required_argument, NULL, 'o'},
     {"help", no_argument, NULL, 'h'},
@@ -241,6 +247,24 @@ QUrl parseCmd(int argc, char* argv[], artemis::Options& options)
 
         case 'b': {
             options.concolicEntryPoint = QString(optarg);
+            break;
+        }
+
+        case 'B': {
+            QStringList features = QString(optarg).split(",");
+            foreach(QString feature, features) {
+                feature = feature.trimmed();
+                if (feature == "radio-restriction") {
+                    options.concolicDisabledFeatures |= artemis::RADIO_RESTRICTION;
+                } else if (feature == "select-restriction") {
+                    options.concolicDisabledFeatures |= artemis::SELECT_RESTRICTION;
+                } else if (feature == "select-restriction-dynamic") {
+                    options.concolicDisabledFeatures |= artemis::SELECT_RESTRICTION_DYNAMIC;
+                } else {
+                    cerr << "ERROR: Invalid choice of concolic-disable-features " << optarg << endl;
+                    exit(1);
+                }
+            }
             break;
         }
 

artemis-code/src/runtime/toplevel/concolicruntime.cpp
@@ -44,6 +44,7 @@ ConcolicRuntime::ConcolicRuntime(QObject* parent, const Options& options, const 
     , mHandlerTracker(options.concolicEventHandlerReport)
     , mNumIterations(0)
     , mExplorationIndex(1)
+    , mDisabledFeatures(options.concolicDisabledFeatures)
 {
     QObject::connect(mWebkitExecutor, SIGNAL(sigExecutedSequence(ExecutableConfigurationConstPtr, QSharedPointer<ExecutionResult>)),
                      this, SLOT(postConcreteExecution(ExecutableConfigurationConstPtr, QSharedPointer<ExecutionResult>)));
@@ -293,6 +294,7 @@ void ConcolicRuntime::postInitialConcreteExecution(QSharedPointer<ExecutionResul
     // Find the form fields on the page and save them.
     mFormFields = permuteFormFields(result->getFormFields(), mOptions.concolicEventHandlerPermutation);
     mFormFieldRestrictions = FormFieldRestrictedValues::getRestrictions(mFormFields, mWebkitExecutor->getPage());
+    mFormFieldInitialRestritions = mFormFieldRestrictions;
 
     // Print the form fields found on the page.
     Log::debug("Form fields found:");
@@ -694,6 +696,16 @@ void ConcolicRuntime::exploreNextTarget(bool isRetry)
 
     // TODO: Currently only select constraints are handled dynamically, so we need to merge them with the static radio button constraints.
     FormRestrictions dynamicRestrictions = mergeDynamicSelectRestrictions(mFormFieldRestrictions, dynamicSelectConstraints);
+    // If these features have been disabled, then remove the restrictions.
+    if (mDisabledFeatures.testFlag(SELECT_RESTRICTION_DYNAMIC)) {
+        dynamicRestrictions.first = mFormFieldInitialRestritions.first;
+    }
+    if (mDisabledFeatures.testFlag(SELECT_RESTRICTION)) {
+        dynamicRestrictions.first.clear();
+    }
+    if (mDisabledFeatures.testFlag(RADIO_RESTRICTION)) {
+        dynamicRestrictions.second.clear();
+    }
 
     Log::info("  Next target:");
     QString targetString = QString("    ") + QString::fromStdString(target->toStatisticsValuesString(true)).trimmed();

artemis-code/src/runtime/toplevel/concolicruntime.h
@@ -134,6 +134,7 @@ protected:
 
     QList<FormFieldDescriptorConstPtr> mFormFields;
     FormRestrictions mFormFieldRestrictions;
+    FormRestrictions mFormFieldInitialRestritions;
     QList<FormFieldDescriptorConstPtr> permuteFormFields(QList<FormFieldDescriptorConstPtr> fields, QString permutation);
     QList<int> mFormFieldPermutation;
 
@@ -152,6 +153,9 @@ protected:
     QMap<QString, InjectionValue> mPreviousInjections;
     QString mPreviousConstraintID;
     void logInjectionValues(TraceClassificationResult classification);
+
+    // Benchmarking
+    QFlags<ConcolicBenchmarkFeatures> mDisabledFeatures;
 
 private slots:
     void postConcreteExecution(ExecutableConfigurationConstPtr configuration, QSharedPointer<ExecutionResult> result);

commit b96dfaf15aaa38e29c4058d686c3bd68217a3aae

Fix issue with radio button DOM constraints introduced by e52d53b9.

artemis-code/src/concolic/solver/constraintwriter/cvc4.cpp
@@ -85,7 +85,6 @@ void CVC4ConstraintWriter::preVisitPathConditionsHook(QSet<QString> varsUsed)
         }
         // else this select is not mentioned in the PC, so ignore.
     }
-
     foreach(RadioRestriction rr, mFormRestrictions.second) {
         QString name;
         bool variableMatch = false;

artemis-code/src/runtime/input/forms/formfieldrestrictedvalues.cpp
@@ -171,4 +171,15 @@ QString FormFieldRestrictedValues::getVariableName(FormFieldDescriptorConstPtr f
 
 
 
+QDebug operator<<(QDebug dbg, const SelectRestriction& sr) {
+    dbg.nospace() << "Select " << sr.variable << " from [" << sr.values.join(",") << "].";
+    return dbg.space();
+}
+
+QDebug operator<<(QDebug dbg, const RadioRestriction& rr) {
+    dbg.nospace() << "Radio " << rr.groupName << " options [" << QStringList(rr.variables.toList()).join(",") << "]" << (rr.alwaysSet ? "" : " NOT") << " always set.";
+    return dbg.space();
+}
+
+
 } // namespace artemis

artemis-code/src/runtime/input/forms/formfieldrestrictedvalues.h
@@ -30,16 +30,22 @@
 namespace artemis
 {
 
-typedef struct {
+struct SelectRestriction {
     QString variable;
-    QList<QString> values;
-} SelectRestriction;
+    QStringList values;
 
-typedef struct {
+    QDebug friend operator<<(QDebug dbg, const SelectRestriction& sr);
+
+};
+
+struct RadioRestriction {
     QString groupName;
     bool alwaysSet; // If no option is set by default, it is possible to submit with all variables set to false.
     QSet<QString> variables;
-} RadioRestriction;
+
+    QDebug friend operator<<(QDebug dbg, const RadioRestriction& rr);
+
+};
 
 typedef QPair<QSet<SelectRestriction>, QSet<RadioRestriction> > FormRestrictions;
 

artemis-code/src/runtime/options.h
@@ -74,7 +74,10 @@ struct ConcolicSearchSelector
 };
 
 enum ConcolicBenchmarkFeatures {
-    RADIO_RESTRICTION, SELECT_RESTRICTION, SELECT_RESTRICTION_DYNAMIC
+    ALL_ENABLED = 0,
+    RADIO_RESTRICTION = 1,
+    SELECT_RESTRICTION = 2,
+    SELECT_RESTRICTION_DYNAMIC = 4
 };
 
 
@@ -103,7 +106,8 @@ typedef struct OptionsType {
         solver(CVC4),
         exportEventSequence(DONT_EXPORT),
         reportHeap(NO_CALLS),
-        heapReportFactor(1)
+        heapReportFactor(1),
+        concolicDisabledFeatures(0)
     {}
 
     QMap<QString, InjectionValue> presetFormfields;

artemis-code/tests/system/fixtures/concolic/radio-button-unsat.html
@@ -1,0 +1,46 @@
+<!--
+TEST PC = ((SymbolicBoolean == ConstantBoolean) == ConstantBoolean)
+TEST Concolic::Solver::Constraint.SYM_IN_testinput_o1: true
+TEST Concolic::Solver::ConstraintsWritten = 3
+TEST Concolic::Solver::ConstraintsSolved = 3
+TEST Concolic::Solver::ConstraintsSolvedAsUNSAT = 1
+-->
+<!doctype html><html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Ben Spencer</title>
+	<meta name="author" content="Ben Spencer">
+	<script type="text/javascript">
+		function validate() {
+			var y = document.getElementById("testinput_o1").checked;
+			var z = document.getElementById("testinput_o2").checked;
+
+			if (y == true) {
+				if (z == true) {
+					// UNSAT, because radio buttons are mutually exclusive.
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	</script>
+
+</head>
+<body>
+
+	<form method="GET" action="about:blank" >
+
+		<input type="radio" name="testinput" value="option1" id="testinput_o1" > Option 1<br>
+		<input type="radio" name="testinput" value="option2" id="testinput_o2" > Option 2<br>
+
+		<button type="submit" onclick="return validate();">Submit</button>
+
+	</form>
+
+</body>
+</html>