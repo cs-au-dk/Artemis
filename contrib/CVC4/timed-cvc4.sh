72657858535582d992c4a5f38692f92527e57069
Casper Svenning Jensen <casper@svenningjensen.dk>
2014-09-19 19:57:53 +0200

    Update solver

--- contrib/CVC4/timed-cvc4.sh
+++ contrib/CVC4/timed-cvc4.sh
@@ -2,5 +2,5 @@
 
 # Run CVC4 with a 10 minute timeout.
 
-timeout 600 ${ARTEMISDIR}/contrib/CVC4/cvc4-2014-05-14-x86_64-linux-opt "$@"
+timeout 600 ${ARTEMISDIR}/contrib/CVC4/cvc4-2014-09-19-x86_64-linux-opt  --strings-exp  --lang=smtlib2 --rewrite-divk "$@"
 
