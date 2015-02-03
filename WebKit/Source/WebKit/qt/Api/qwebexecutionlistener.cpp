cs-au-dk/Artemis

commit a46c578fd2883c8d26bd911a58fe8e31d760d478

Improve precision of visibility analysis

WebKit/Source/WebKit/qt/Api/qwebexecutionlistener.cpp
@@ -44,6 +44,7 @@
 #include "JavaScriptCore/bytecode/CodeBlock.h"
 #include "JavaScriptCore/bytecode/Opcode.h"
 #include "JavaScriptCore/interpreter/Interpreter.h"
+#include "WebCore/dom/Node.h"
 
 #include "qwebexecutionlistener.h"
 
@@ -60,11 +61,18 @@ QWebExecutionListener::QWebExecutionListener(QObject *parent)
 {
 }
 
-void QWebExecutionListener::eventAdded(WebCore::EventTarget * target, const char* type) {
+void QWebExecutionListener::eventAdded(WebCore::EventTarget* target, const char* type) {
     std::string typeString = std::string(type);
 
     if (target->toNode() != NULL) {
-        emit addedEventListener(new QWebElement(target->toNode()), QString(tr(typeString.c_str())));
+        WebCore::Node*  node = target->toNode();
+        if (node->isDocumentNode() || node->isElementNode()) {
+            // Notice! QWebElement will default to a NULL value if node is not an element node but a document node.
+            // We recover this case later by assuming null values refer to the target node.
+            emit addedEventListener(new QWebElement(node), QString(tr(typeString.c_str())));
+        } else {
+            qWarning() << QString::fromStdString("Event handler added to non-document and non-elmenet node. Ignored.");
+        }
 
     } else if (target->toDOMWindow() != NULL) {
         emit addedEventListener(new QWebElement(target->toDOMWindow()->frameElement()), QString(tr(typeString.c_str())));

WebKit/Source/WebCore/dom/Element.cpp
@@ -73,6 +73,7 @@
 #include "htmlediting.h"
 #include <wtf/text/CString.h>
 #include <sstream>
+#include <iostream>
 
 #if ENABLE(SVG)
 #include "SVGElement.h"

WebKit/Source/WebKit/qt/Api/qwebelement.cpp
@@ -62,6 +62,7 @@
 
 #ifdef ARTEMIS
 #include "interpreter/Interpreter.h"
+#include "WebCore/dom/Element.h"
 
 #endif
 
@@ -827,8 +828,9 @@ QString QWebElement::xPath(){
 bool QWebElement::isUserVisible(){
     // this element is (approximative) visible by the user if
     return (m_element != NULL && // it exist
-            (tagName().length() == 0 || // is the root element
-             parent().isNull() || // is the root element
+            (tagName().length() == 0 || // root element
+             tagName().compare(QString::fromStdString("BODY")) == 0 || // body is always visible! (but has no bounding box for some reason)
+             parent().isNull() || // root element
              m_element->hasNonEmptyBoundingBox())); // has a bounding box when rendered
 }
 

artemis-code/src/runtime/browser/executionresultbuilder.cpp
@@ -81,19 +81,7 @@ void ExecutionResultBuilder::registerEventHandlersIntoResult()
     QPair<QWebElement*, QString> p;
     foreach(p, mElementPointers) {
         if (getType(p.second) == UNKNOWN_EVENT) {
-            qWarning() << "Ignoring unsupported event of type " << p.second;
-            continue;
-        }
-
-        if (mEnableEventVisibilityFiltering && !p.first->isUserVisible()) {
-            Statistics::statistics()->accumulate("WebKit::events::skipped::visibility", 1);
-            qDebug() << "Skipping EVENTHANDLER event (not user visible) =" << p.second
-                     << "tag = " << p.first->tagName()
-                     << "id = " << p.first->attribute(QString("id"))
-                     << "title = " << p.first->attribute(QString("title"))
-                     << "class = " << p.first->attribute("class")
-                     << "visible = " << p.first->isUserVisible()
-                     << "xpath = " << p.first->xPath();
+            qWarning() << "Ignoring unsupported event of type " << p.second << ", skipping";
             continue;
         }
 
@@ -106,11 +94,30 @@ void ExecutionResultBuilder::registerEventHandlersIntoResult()
         EventHandlerDescriptorConstPtr handler = EventHandlerDescriptorConstPtr(new EventHandlerDescriptor(p.first, p.second));
 
         if (handler->isInvalid()) {
-            qWarning() << "element was invalid, ignoring";
-        } else {
-            Statistics::statistics()->accumulate("WebKit::events::added", 1);
-            mResult->mEventHandlers.append(handler);
+            qWarning() << "element was invalid, skipping";
+            continue;
         }
+
+        // sometimes a handler is registered on a NULL element? and the descriptor infrastructure guesses a correct element to replace it
+        // check if that guess is visible
+        if (mEnableEventVisibilityFiltering) {
+            QWebElement actualSource = handler->getDomElement()->getElement(mPage);
+            if (actualSource.isUserVisible() == false) {
+                Statistics::statistics()->accumulate("WebKit::events::skipped::visibility", 1);
+                qDebug() << "Skipping EVENTHANDLER event (not user visible) =" << p.second
+                         << "tag = " << actualSource.tagName()
+                         << "id = " << actualSource.attribute(QString("id"))
+                         << "title = " << actualSource.attribute(QString("title"))
+                         << "class = " << actualSource.attribute("class")
+                         << "visible = " << actualSource.isUserVisible()
+                         << "xpath = " << actualSource.xPath();
+                continue;
+            }
+        }
+
+        Statistics::statistics()->accumulate("WebKit::events::added", 1);
+        mResult->mEventHandlers.append(handler);
+
     }
 
 }
@@ -202,7 +209,8 @@ void ExecutionResultBuilder::slEventListenerAdded(QWebElement* elem, QString eve
              << "title = " << elem->attribute(QString("title"))
              << "class = " << elem->attribute("class")
              << "visible = " << elem->isUserVisible()
-             << "xpath = " << elem->xPath();
+             << "xpath = " << elem->xPath()
+             << "isNull = " << elem->isNull();
 
     if (isNonInteractive(eventName)) {
         return;

artemis-code/tests/system/harness/artemis.py
@@ -144,7 +144,7 @@ def execute_artemis(execution_uuid, url, iterations=1,
     cmd = timeout + [ARTEMIS_EXEC] + [url] + args
 
     if dryrun or verbose:
-        print ' '.join(cmd)
+        print(' '.join(cmd))
 
     if dryrun:
         return

commit f22671cf7bd86e2ccf8c85aa084f4fd51bf9d356

Fix a build error introduced in a46c578f

WebKit/Source/WebKit/qt/Api/qwebexecutionlistener.cpp
@@ -45,6 +45,7 @@
 #include "JavaScriptCore/bytecode/Opcode.h"
 #include "JavaScriptCore/interpreter/Interpreter.h"
 #include "WebCore/dom/Node.h"
+#include "WebCore/dom/Document.h"
 
 #include "qwebexecutionlistener.h"
 