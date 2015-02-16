b432f08033c8b34f344b8922fd3e2aed21230640
Ben Spencer <benjamin.spencer@balliol.ox.ac.uk>
2015-02-16 12:38:13 +0000

    Server mode deployment must include our WebKit lib as well.

--- artemis-code/scripts/deploy-server-mode.sh
+++ artemis-code/scripts/deploy-server-mode.sh
@@ -25,7 +25,7 @@ function copy_in_file {
     FILE=$1
     NAME=$2
     if [[ -e "$FILE" ]]; then
-        cp -r "$FILE" "$TARGET_DIR/$NAME"
+        cp -rL "$FILE" "$TARGET_DIR/$NAME"
     else
         echo "Could not find $FILE"
         echo "Are you running from the Artemis directory root?"
@@ -38,14 +38,20 @@ function copy_in_file {
 # Copy all the files
 copy_in_file ./artemis-code/dist/artemis artemis
 copy_in_file ./docs/sections/server.rst server.rst
+
 mkdir "$TARGET_DIR/tests"
 copy_in_file ./artemis-code/tests/system/server.py tests/server.py
 copy_in_file ./artemis-code/tests/system/fixtures/server tests/fixtures
 
+mkdir "$TARGET_DIR/lib"
+copy_in_file ./WebKit/WebKitBuild/Release/lib/libQtWebKit.so.4 lib/libQtWebKit.so.4
+
+# Fetch git info while still in the Artemis dir.
 COMMIT=$(git show --no-patch --format="%h - %s")
 COMMIT_SHA=$(git rev-parse HEAD)
-COMMIT_TITLE=$()
 BRANCH=$(git symbolic-ref --short HEAD)
+git diff-index --quiet HEAD --
+GIT_CHANGES=$?
 
 cd "$TARGET_DIR"
 
@@ -63,10 +69,25 @@ sed -i "s/^ARTEMIS_EXEC.*/ARTEMIS_EXEC = os.path.join(os.path.dirname(os.path.re
 # Add info linking this deploy to the current git commit.
 VERSION_FILE="version.txt"
 echo "Branch: $BRANCH" >> "$VERSION_FILE"
-echo "Commit: $COMMIT" >> "$VERSION_FILE"
+    echo "Commit: $COMMIT" >> "$VERSION_FILE"
+if [[ $GIT_CHANGES -ne 0 ]]; then
+    echo "        [With changes]" >> "$VERSION_FILE"
+fi
 echo "GitHub: https://github.com/cs-au-dk/Artemis/commit/$COMMIT_SHA" >> "$VERSION_FILE"
 
 # Set SVN ignore.
 svn propset svn:ignore --recursive .output . > /dev/null
+
+# Add wrapper script.
+WRAPPER_FILE=run-artemis.sh
+cat << 'EOF' > "$WRAPPER_FILE"
+#!/bin/bash
+
+# Runs the artemis server with the correct WebKit lib linked in and appropriate arguments.
+
+LD_PRELOAD=$(dirname $0)/lib/libQtWebKit.so.4 $(dirname $0)/artemis --major-mode server --analysis-server-port 5500 -v all
+
+EOF
+chmod +x "$WRAPPER_FILE"
 
 echo "Done."
